247238d12640f7ff671c46d76f127c3739e3e573
Alexander <alexmorris@catalyst.net.nz>
2020-10-05 13:52:52 +1300

    login/index.php: noredirect login, don't deprecate alternativeloginurl
    ff66ed18e4d1f873f15b75c9ae356c7bb600cfb6 and 66365495db9b4c985434b88835af68d75d9c1f18 in elearning-local/totara-local
    WR #344598

--- server/login/index.php
+++ server/login/index.php
@@ -46,6 +46,8 @@ redirect_if_major_upgrade_required();
 
 $testsession = optional_param('testsession', 0, PARAM_INT); // test session works properly
 $anchor      = optional_param('anchor', '', PARAM_RAW);      // Used to restore hash anchor to wantsurl.
+$noredirect  = optional_param('magicpony', 0, PARAM_BOOL); // don't redirect
+$noredirect |= optional_param('noredirect', 0, PARAM_BOOL); // don't redirect
 
 $context = context_system::instance();
 $PAGE->set_url("$CFG->wwwroot/login/index.php");
@@ -340,7 +342,7 @@ if (empty($SESSION->wantsurl)) {
 }
 
 /// Redirect to alternative login URL if needed
-if ((!empty($CFG->allowlogincsrf) || $authsequence[0] == 'shibboleth') && !empty($CFG->alternateloginurl)) { // Totara: alternate login url is deprecated!
+if ((!empty($CFG->allowlogincsrf) || $authsequence[0] == 'shibboleth') && !empty($CFG->alternateloginurl) && empty($noredirect)) { // Totara: alternate login url is deprecated!
     $loginurl = new moodle_url($CFG->alternateloginurl);
 
     $loginurlstr = $loginurl->out(false);
