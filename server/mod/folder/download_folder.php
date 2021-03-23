b947da7ca92de1dfab31848706434ede05db2f02
Alex Morris <alexmorris@catalyst.net.nz>
2021-03-24 11:10:59 +1300

    mod_folder: Increase max execution time on downloads
    WR #353487

--- server/mod/folder/download_folder.php
+++ server/mod/folder/download_folder.php
@@ -52,6 +52,8 @@ folder_downloaded($folder, $course, $cm, $context);
 // Close the session.
 \core\session\manager::write_close();
 
+core_php_time_limit::raise(120);
+
 $filename = shorten_filename(clean_filename($folder->name . "-" . date("Ymd")) . ".zip");
 $zipwriter = \core_files\archive_writer::get_stream_writer($filename, \core_files\archive_writer::ZIP_WRITER);
 
