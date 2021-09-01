58d3d24d12cb3338a844bc8f7fcdb7788809586c
Tatsuhiro Kirihara <tatsuhiro.kirihara@totaralearning.com>
2021-09-01 13:46:35 +1200

    TL-31979 totara_comment: Deprecate interactor

---
+++ client/component/totara_comment/src/tests/unit/components/box/SidePanelCommentBox.spec.js
@@ -0,0 +1,54 @@
+/**
+ * This file is part of Totara Enterprise Extensions.
+ *
+ * Copyright (C) 2021 onwards Totara Learning Solutions LTD
+ *
+ * Totara Enterprise Extensions is provided only to Totara
+ * Learning Solutions LTD's customers and partners, pursuant to
+ * the terms and conditions of a separate agreement with Totara
+ * Learning Solutions LTD or its affiliate.
+ *
+ * If you do not have an agreement with Totara Learning Solutions
+ * LTD, you may not access, use, modify, or distribute this software.
+ * Please contact [licensing@totaralearning.com] for more information.
+ *
+ * @author Tatsuhiro Kirihara <tatsuhiro.kirihara@totaralearning.com>
+ * @module totara_comment
+ */
+
+import { shallowMount } from '@vue/test-utils';
+import component from 'totara_comment/components/box/SidePanelCommentBox.vue';
+
+describe('totara_comment/components/box/SidePanelCommentBox', () => {
+  const template = {
+    component: '?',
+    area: '?',
+    instanceId: '?',
+  };
+
+  it('check reallyCommentAble without showComment or interactor', () => {
+    const propsData = Object.assign({}, template);
+    const wrapper = shallowMount(component, { propsData });
+    expect(wrapper.vm.reallyCommentAble).toBeTrue();
+  });
+
+  it.each([true, false])(
+    'check reallyCommentAble with showComment',
+    showComment => {
+      const propsData = Object.assign({}, template, { showComment });
+      const wrapper = shallowMount(component, { propsData });
+      expect(wrapper.vm.reallyCommentAble).toBe(showComment);
+    }
+  );
+
+  it.each([true, false])(
+    'check reallyCommentAble with interactor',
+    canComment => {
+      const propsData = Object.assign({}, template, {
+        interactor: { can_comment: canComment },
+      });
+      const wrapper = shallowMount(component, { propsData });
+      expect(wrapper.vm.reallyCommentAble).toBe(canComment);
+    }
+  );
+});
