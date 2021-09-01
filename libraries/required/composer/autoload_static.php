d5659dc4da4bc3e806abfb8bb539f4d816241448
Yuliya Bozhko <yuliya.bozhko@totaralearning.com>
2021-09-02 09:30:38 +1200

    TL-28246 libraries: Update PHPMailer to v6.5.1
    Signed-off-by: Yuliya Bozhko <yuliya.bozhko@totaralearning.com>

--- libraries/required/composer/autoload_static.php
+++ libraries/required/composer/autoload_static.php
@@ -7,10 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit51970ff591fae91dcbec35ebf2b84603
 {
     public static $files = array (
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
         '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
         'b686b8e46447868025a15ce5d0cb2634' => __DIR__ . '/..' . '/symfony/polyfill-php74/bootstrap.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
-        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
     );
 
     public static $prefixLengthsPsr4 = array (
