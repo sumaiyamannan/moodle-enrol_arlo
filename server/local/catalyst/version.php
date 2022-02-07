2d63a57b73cc36d9923d4db22432cf30453104b9
Sumaiya Javed <sumaiya.javed@catalyst.net.nz>
2022-02-08 09:37:23 +1300

    WR373403: Totara HR Import delete logs. Final version bump.

--- server/local/catalyst/version.php
+++ server/local/catalyst/version.php
@@ -24,6 +24,6 @@
 defined('MOODLE_INTERNAL') or die();
 
 $plugin->component = 'local_catalyst';
-$plugin->version   = 2021123101;
+$plugin->version   = 2022021500;
 $plugin->requires  = 2013111800;
 $plugin->cron      = 0;
